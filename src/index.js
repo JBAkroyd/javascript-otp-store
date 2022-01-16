93de53cf66a159322e4128d8f1b1746c8f016c6d
Jordan Akroyd <36215472+JBAkroyd@users.noreply.github.com>
2022-01-16 15:48:16 +1300

    take a screenshot of extension dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import html2canvas from "html2canvas";
 console.log("loaded index.js");
 window.onload = () => {
     let button = document.getElementById("triggerScreenshot");
     button.addEventListener("click", () => {
         console.log("clicked button");
+        html2canvas(document.body).then(function(canvas) {
+            document.body.appendChild(canvas);
+        });
     });
 }
